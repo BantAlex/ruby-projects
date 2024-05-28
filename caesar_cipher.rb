252d4e562812ca475a410fc893a045d49919abd2
BantAlex <bantalex3@gmail.com>
2024-05-28 21:26:50 +0300

    Finish caesar_cipher project

---
+++ caesar_cipher.rb
@@ -0,0 +1,22 @@
+def caesar_cipher (str,shift)
+  encrypted_str = []
+  str_array = str.split("")
+  lower_ab = ("a".."z").to_a
+  upper_ab = ("A".."Z").to_a
+  regex = ["!","?","-","."," "]
+  str_array.each do |letter|
+    if lower_ab.include?(letter)
+       index_in_ab = (lower_ab.index(letter) + shift) % 26
+       encrypted_str.push(lower_ab[index_in_ab])
+    elsif upper_ab.include?(letter)
+      index_in_ab = (upper_ab.index(letter) + shift) % 26
+       encrypted_str.push(upper_ab[index_in_ab])
+    elsif regex.include?(letter)
+      encrypted_str.push(letter)
+    end
+  end
+  encrypted_str.join("")
+end
+
+p caesar_cipher("What a string!", 5) #=> "Bmfy f xywnsl!"
+
