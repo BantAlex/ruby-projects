0b93c5fdff1146bb0fd3ce712113204f0da93eb5
BantAlex <bantalex3@gmail.com>
2024-06-01 20:47:55 +0300

    Another aproach
    Did it with include, took some help from other's code. Simpler than the way I did it

--- odin-first-set/sub_strings.rb
+++ odin-first-set/sub_strings.rb
@@ -1,27 +1,13 @@
 def substrings (str,dict)
-  result = {}
-  regex = [".",",","!","?"]
-  split_str = str.split(" ")
-
+  result = Hash.new(0)
   dict.each do |word|
-    word = word.downcase
-    split_str.each do |str_word,counter|
-      str_word = str_word.downcase
-      counter = 0
-      if str_word == word
-        counter += 1
-        result[word] = counter
-      end
+    if str.include?(word)
+      result[word] += 1
     end
   end
- p result
+   result
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 substrings("Howdy partner, sit down! How's it going?", dictionary)
 
-#!Multiple word feature:
-  #*Split the str into words
-  #*Make regex for commas and such(?)
-  #*Compare with dict
-  #*Push into result with counter as value of str's key
