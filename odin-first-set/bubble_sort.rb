b03a47019e351e5a76766cd4b1108a13654a8c64
BantAlex <bantalex3@gmail.com>
2024-06-04 20:53:39 +0300

    Did it comparing with .sort
    Don't think that's a valid solution though

--- odin-first-set/bubble_sort.rb
+++ odin-first-set/bubble_sort.rb
@@ -1,7 +1,16 @@
 def bubble_sort (arr)
   arr.each_with_index do |num,index|
-    if arr[index] < arr[index + 1]
-      arr[index] = num
+    next_num = arr[index + 1]
+    if next_num != nil
+      if num > next_num
+        arr[index] = arr[index + 1]
+        arr[index + 1] = num
+      end
     end
+  end
+  if arr != arr.sort
+    bubble_sort(arr)
+  else
+   p arr
   end
 end
